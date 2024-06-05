94fc310cb5a0c74f9ac530b02cdde61b33005eb9
Sam Shacter <sshacter@gmail.com>
2024-06-05 11:23:56 -0500

    Complete
    :
    
    :wq

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import storeInstance from './components/Store/Store';
+
 import App from './components/App/App';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+<Provider store={storeInstance}>
         <App />
+        </Provider>
     </React.StrictMode>
 );
